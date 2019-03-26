ea23fd6b0bd8c55efa8792e9fbae1ef7cb949d40
Anthony Eriksen <ajrayid@gmail.com>
2019-03-26 15:59:52 -0700

    Added Search functionality
    Added search functionality with javascript.

--- js/main.js
+++ js/main.js
@@ -1,8 +1,13 @@
-$(document).ready(function(){
-    $("#myInput").on("keyup", function() {
-      let value = $(this).val().toLowerCase();
-      $(".example-image-link").filter(function() {
-        $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
-      });
-    });
-  });
+const input = document.getElementById("myInput");
+const entries = document.getElementsByClassName("example-image-link");
+input.addEventListener("keyup",function() {
+   const val = input.value; 
+    for (let i = 0; i < entries.length; i++) {
+      const caption = entries[i].getAttribute("data-title"); 
+      if (caption.toLowerCase().indexOf(val.toLowerCase()) != -1) {
+        entries[i].style.display = "block";
+      } else entries[i].style.display = "none";
+    }
+  },
+  false
+);
